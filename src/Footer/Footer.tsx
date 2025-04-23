fd1d0cbf4514c064bef57a027d1bda0c527db61d
Dylan-Lvrd <dylan.louvard@oclock.school>
2025-04-23 15:30:32 +0200

    first components

---
+++ src/Footer/Footer.tsx
@@ -0,0 +1,15 @@
+function Footer (){
+    return (
+        <div className="footer-links">
+        <a href="#">Mentions Légales</a>
+        <a href="#">Confidentialité</a>
+        <a href="#">Cookies</a>
+        <a href="#">Contact</a>
+    
+    <a className="copyright"/>
+        ©2025 Blabla Book - Tous droits réservés
+    </div>
+    )
+}
+
+export default Footer;
