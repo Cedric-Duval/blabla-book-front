2cf80a57e0d5492cebd353c3f43d95a9aa9df064
Dylan-Lvrd <dylan.louvard@oclock.school>
2025-04-24 11:31:59 +0200

    welcoming page first div

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import Navbar from './Navbar/Navbar'
 import Footer from './Footer/Footer'
 
 import './App.scss'
+import Homepage from './Homepage/Homepage'
 
 function App() {
 
   return (
     <div className='app'>
       <Navbar />
+      <Homepage />
       texte
       <Footer />
     </div>
--- src/Navbar/Navbar.tsx
+++ src/Navbar/Navbar.tsx
@@ -5,6 +5,7 @@ function Navbar (){
 
         <nav className="navbar">
             <div id="logo">
+               
                 <a href=""><img src="/Logo.png" alt="" className='header-logo' /></a>
             </div>
             <input
@@ -14,8 +15,11 @@ function Navbar (){
                         placeholder="Recherche par titre, auteur, ISBN ..."/>
             <div id="menu">
                 <ul>
+                
                 <li><a href="#">Accueil</a></li>
+               
                 <li><a href="#">Livres</a></li>
+                
                 <li><a href="#">Bibliothèque</a></li>
                 <li><a href="#" className="button-connect">Se connecter</a></li>
                 <li><a href="#" className="create-account">Créer un compte</a></li>
