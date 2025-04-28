a506273b1c1aab21093dfabda420f2aba65979d3
Cédric Duval <cedric.duval@oclock.school>
2025-04-28 11:16:35 +0200

    affichage dynamique d'un livre

--- src/Book/Book.tsx
+++ src/Book/Book.tsx
@@ -1,22 +1,50 @@
-import './Book.scss'
+import './Book.scss';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+import axios from 'axios'
 
 function Book() {
 
+  const params = useParams();
+  const bookId = params.id;
+  console.log(bookId);
+
+  const [book, setBook] = useState<IBook[]>([]);
+
+    useEffect(() => {
+        const getBook = async () => {
+            try {
+                const response = await axios.get(
+                    `http://localhost:3000/book/${bookId}`,
+                );
+                setBook(response.data);
+            } catch (_error) {
+            }
+        };
+        getBook();
+    }, []);
+
+    console.log(book);
+    
+
+
+
+
   return (
     <section id="book">
-        <h1></h1>
+        <h1>{book.title}</h1>
         <div id="main">
-            <img src="" alt=""/>
+            <img src={`${book.image}`} alt={`${book.title}`}/>
             <div id="details">
-                <h2></h2>
-                <p></p>
-                <p></p>
-                <p></p>
-                <p></p>
-                <p></p>
+                <h2>{book.title}</h2>
+                <p>{book.author}</p>
+                <p>{book.publication_year}</p>
+                <p>{book.editor}</p>
+                <p>{book.isbn}</p>
+                <p>{book.pages} pages</p>
                 <h3>Résumé:</h3>
-                <p></p>
-                <button></button>
+                <p>{book.summary}</p>
+                <button>Bouton</button>
 
             </div>
         </div>
