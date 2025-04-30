02a592a5f1c737f0e8f6a97a5ecaa046f03cc77e
Dylan-Lvrd <dylan.louvard@oclock.school>
2025-04-30 10:18:48 +0200

    mise en place titre

--- src/Navbar/Navbar.tsx
+++ src/Navbar/Navbar.tsx
@@ -29,13 +29,15 @@ function Navbar({
           <img src="../Pictures/Logo.png" alt="" className="header-logo" />
         </Link>
       </div>
-      {isLogged && (
+      {isLogged ? (
         <input
           type="text"
           id="search"
           name="search"
           placeholder="Recherche par titre, auteur, ISBN ..."
         />
+      ) : (
+        <h1 className="title-blablabook">BlaBla Book</h1>
       )}
 
       <div id="menu">
