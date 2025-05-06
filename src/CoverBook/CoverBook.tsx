d7b7283de85b15ed708fb479dba80a08a545e53d
william <william.dodart@gmail.com>
2025-05-06 20:25:01 +0200

    Finalisation of the library modal

--- src/CoverBook/CoverBook.tsx
+++ src/CoverBook/CoverBook.tsx
@@ -1,42 +1,50 @@
 import { Link } from 'react-router';
-import './CoverBook.scss'
+import './CoverBook.scss';
 import type { IBooks } from '../@types/books';
 import type { ILibrary } from '../@types/books';
 
 interface CoverBookProps {
-    book:  IBooks | null | undefined;
-    setDisplayModalLibrary: React.Dispatch<React.SetStateAction<boolean>>;
-    setCurrentBook: React.Dispatch<React.SetStateAction<ILibrary | null | undefined>>;
+  book: IBooks;
+  setDisplayModalLibrary: React.Dispatch<React.SetStateAction<boolean>>;
+  setCurrentBook: React.Dispatch<
+    React.SetStateAction<ILibrary | null | undefined>
+  >;
 }
 
-function CoverBook({ book, setDisplayModalLibrary, setCurrentBook }: CoverBookProps) {
-    return (<><li key={book.id} className='books-list-li library-menu-list'>
-
-
+function CoverBook({
+  book,
+  setDisplayModalLibrary,
+  setCurrentBook,
+}: CoverBookProps) {
+  return (
+    <>
+      <li key={book.id} className="books-list-li library-menu-list">
         <Link to={`/book/${book.id}`}>
-            <figure>
-                <div id="book-img">
-                    <button className="test-btn" type='button' onClick={(event) => {
-                        setDisplayModalLibrary(true);
-                        event.preventDefault();
-                        setCurrentBook(book);
-                    }}> ... </button>
-                    <img
-                        src={book.image} alt="book-image"
-                    />
-
-                </div>
-                <hgroup>
-                    <figcaption>{book.title}</figcaption>
-                    <h5>{book.author}</h5>
-                </hgroup>
-
-            </figure>
+          <figure>
+            <div id="book-img">
+              <button
+                className="test-btn"
+                type="button"
+                onClick={(event) => {
+                  setDisplayModalLibrary(true);
+                  event.preventDefault();
+                  setCurrentBook(book);
+                }}
+              >
+                {' '}
+                ...{' '}
+              </button>
+              <img src={book.image} alt="book-image" />
+            </div>
+            <hgroup>
+              <figcaption>{book.title}</figcaption>
+              <h5>{book.author}</h5>
+            </hgroup>
+          </figure>
         </Link>
-
-
-
-    </li></>)
-};
+      </li>
+    </>
+  );
+}
 
 export default CoverBook;
--- src/ModalLibrary/ModalLibrary.tsx
+++ src/ModalLibrary/ModalLibrary.tsx
@@ -1,48 +1,99 @@
 import type { ILibrary } from '../@types/books';
 import api from '../features/axiosApi';
-import './ModalLibrary.scss'
+import './ModalLibrary.scss';
 
 type IModalLibraryProps = {
-    closeModalLibrary: () => void;
-    currentBook: ILibrary | null | undefined;
-    setMyLibraries: React.Dispatch<React.SetStateAction<ILibrary[]>>;
-}
+  closeModalLibrary: () => void;
+  currentBook: ILibrary | null | undefined;
+  setMyLibraries: React.Dispatch<React.SetStateAction<ILibrary[]>>;
+};
 
-function ModalLibrary({ closeModalLibrary, currentBook, setMyLibraries }: IModalLibraryProps) {
+function ModalLibrary({
+  closeModalLibrary,
+  currentBook,
+  setMyLibraries,
+}: IModalLibraryProps) {
+  async function editBookStatus() {
+    const response = await api.patch(
+      `/library/${currentBook?.LibraryBook.library_id}/book/${currentBook?.id}`,
+    );
+    setMyLibraries(response.data);
+  }
 
-    async function editBookStatus() {
-        const response = await api.patch(`/library/${currentBook?.LibraryBook.library_id}/book/${currentBook?.id}`);
-        setMyLibraries(response.data);
-    }
-
-
-    return (
-        <div className="hidden-background" onClick={closeModalLibrary}>
-            <div className='library'>
-                <div onClick={closeModalLibrary} className='library-closeBtn'><img src="../public/Pictures/gridicons--cross.svg" alt="close-button" /></div>
-                <ul className='library-menu'>
-                    <li className={currentBook?.LibraryBook.read ? 'library-menu-li selected' : 'library-menu-li'}
-                        onClick={(event) => {
-                            if (!currentBook.LibraryBook.read) {
-                                editBookStatus();
-                                return;
-                            }
-                            event.stopPropagation();
-                        }}> <img className="library-menu-li-img" src="../public/Pictures/ph--book-open.svg" alt="" /><p className='library-menu-li-text'>Livre lus</p></li>
-                    <li className={!currentBook?.LibraryBook.read ? 'library-menu-li selected' : 'library-menu-li'}
-                        onClick={(event) => {
-                            if (currentBook.LibraryBook.read) {
-                                editBookStatus();
-                                return;
-                            }
-                            event.stopPropagation();
-                        }}> <img className="library-menu-li-img" src="../public/Pictures/tdesign--time.svg" alt="" /><p className='library-menu-li-text'>A lire </p></li>
-                    <li className='library-menu-li'><img className="library-menu-li-img" src="../public/Pictures/stash--star-duotone.svg" alt="" /><p className='library-menu-li-text'>Noter</p></li>
-                    <li className='library-menu-li'><img className="library-menu-li-img" src="../public/Pictures/mdi--dialogue-outline.svg" alt="" /><p className='library-menu-li-text'>Laisser un avis</p></li>
-                </ul>
-            </div>
+  return (
+    <div className="hidden-background" onClick={closeModalLibrary}>
+      <div className="library">
+        <div onClick={closeModalLibrary} className="library-closeBtn">
+          <img
+            src="../public/Pictures/gridicons--cross.svg"
+            alt="close-button"
+          />
         </div>
-    )
+        <ul className="library-menu">
+          <li
+            className={
+              currentBook?.LibraryBook.read
+                ? 'library-menu-li selected'
+                : 'library-menu-li'
+            }
+            onClick={(event) => {
+              if (!currentBook.LibraryBook.read) {
+                editBookStatus();
+                return;
+              }
+              event.stopPropagation();
+            }}
+          >
+            {' '}
+            <img
+              className="library-menu-li-img"
+              src="../public/Pictures/ph--book-open.svg"
+              alt=""
+            />
+            <p className="library-menu-li-text">Livre lus</p>
+          </li>
+          <li
+            className={
+              !currentBook?.LibraryBook.read
+                ? 'library-menu-li selected'
+                : 'library-menu-li'
+            }
+            onClick={(event) => {
+              if (currentBook.LibraryBook.read) {
+                editBookStatus();
+                return;
+              }
+              event.stopPropagation();
+            }}
+          >
+            {' '}
+            <img
+              className="library-menu-li-img"
+              src="../public/Pictures/tdesign--time.svg"
+              alt=""
+            />
+            <p className="library-menu-li-text">A lire </p>
+          </li>
+          <li className="library-menu-li">
+            <img
+              className="library-menu-li-img"
+              src="../public/Pictures/stash--star-duotone.svg"
+              alt=""
+            />
+            <p className="library-menu-li-text">Noter</p>
+          </li>
+          <li className="library-menu-li">
+            <img
+              className="library-menu-li-img"
+              src="../public/Pictures/mdi--dialogue-outline.svg"
+              alt=""
+            />
+            <p className="library-menu-li-text">Laisser un avis</p>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default ModalLibrary;
