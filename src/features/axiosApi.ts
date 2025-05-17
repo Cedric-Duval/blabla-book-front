0fffff30ae5bbd2b210ae63ad3a030cc15634890
Cédric Duval <cedric.duval@oclock.school>
2025-05-17 15:23:52 +0200

    corrections TS User et package pour le build

--- src/features/axiosApi.ts
+++ src/features/axiosApi.ts
@@ -16,7 +16,7 @@
 
 import axios from 'axios';
 
-const apiDevPort = import.meta.env.VITE_API_DEV_PORT;
+//const apiDevPort = import.meta.env.VITE_API_DEV_PORT;
 
 const api = axios.create({
   baseURL: 'http://localhost:3000', //apiDevPort
