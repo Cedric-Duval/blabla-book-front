c9cb6d22a951487193aa72b6f3dbb3bfce9daaeb
Cédric Duval <cedric.duval@oclock.school>
2025-05-19 16:04:58 +0200

    modif variables environnement

--- src/features/axiosApi.ts
+++ src/features/axiosApi.ts
@@ -1,25 +1,9 @@
-// import axios from 'axios';
-
-// const api = axios.create({
-//   baseURL: 'http://localhost:3000',
-// });
-
-// api.interceptors.request.use((config) => {
-//   const token = localStorage.getItem('token');
-//   if (token) {
-//     config.headers.Authorization = `Bearer ${token}`;
-//   }
-//   return config;
-// });
-
-// export default api;
-
 import axios from 'axios';
 
-//const apiDevPort = import.meta.env.VITE_API_DEV_PORT;
+const apiDevPort = import.meta.env.VITE_API_DEV_PORT;
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000', //apiDevPort
+  baseURL: apiDevPort, // 'http://localhost:3000',
 });
 
 api.interceptors.request.use((config) => {
